3002313efccf55f06ded5114165d6004f13c3ee8
MalteDs <dmalte2002@hotmail.com>
2025-09-10 13:31:45 -0500

    feat: add UserRequest and UserResponse DTOs

---
+++ demo/src/main/java/com/example/demo/dto/user/UserRequest.java
@@ -0,0 +1,26 @@
+package com.example.demo.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserRequest {
+    @NotBlank(message = "name is required")
+    private String name;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "invalid email format")
+    private String email;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, message = "password must have at least 8 characters")
+    private String password;
+}
